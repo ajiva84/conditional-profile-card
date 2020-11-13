7dc18984090ebd2452918770330961dceac33718
ajiva84 <jivazam@gmail.com>
2020-11-13 02:05:47 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -37,6 +37,11 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  if (variables.name == null) variables.name = "first name";
+  if (variables.lastname == null) variables.lastname = "Last name";
+  if (variables.city == null) variables.city = "City";
+  if (variables.country == null) variables.country = "country";
+  if (variables.role === null) variables.role = "your role";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
