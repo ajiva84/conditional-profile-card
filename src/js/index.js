1318ae7620fda827933f06038e9a914c57b9d49b
ajiva84 <jivazam@gmail.com>
2020-11-13 01:41:50 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -23,9 +23,17 @@ import "../style/index.scss";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
+  console.log(
+    `"These are the current variables: ${variables.name} "`,
+    variables
+  ); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  variables.avatarURL =
+    "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/67964582_2329615054020024_710734551101472768_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=BYjg1ElTPTsAX8CSbzj&_nc_tp=25&oh=aae1a31a1ffdcb12d8dfff0151dae5cd&oe=5FD78D4C";
+  variables.background =
+    "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/118471801_748904155888310_3842231648044288454_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=i9K5WWWYjiYAX9YRv5s&tp=15&oh=98a600deeefbe9941759576ff6a9fef2&oe=5FD66EB8";
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
@@ -33,14 +41,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
